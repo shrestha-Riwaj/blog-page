6d97af23fce2a0e2af04e4a5e2f139a3d9fb8225
Riwaj <shresthariwaj10@gmail.com>
2024-06-24 13:20:34 +0545

    Creating a model for our blog page
    making necessary migration to our db

--- blogpage/models.py
+++ blogpage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    body =models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-publish', )
+        db_table = "posts"
+
+    def __str__(self):
+        return self.title
